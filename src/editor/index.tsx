7bc5d4ff72d562f97a11982980eccd9550a904ec
John Rees <johnrees@users.noreply.github.com>
2017-11-30 20:12:47 +0000

    added react

---
+++ src/editor/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const rootEl = document.querySelector("main");
+
+ReactDOM.render(<h1>Main</h1>, rootEl);
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: "source-map", // - Approach that plays well with UglifyJsPlugin; typically you might use this in Production
+  context: path.resolve(__dirname, "./src"),
+  entry: {
+    editor: "./editor/index.tsx"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist/js/"),
+    publicPath: "/js/"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./public"),
+    hot: false,
+    inline: false,
+    host: '0.0.0.0'
+  },
+  // plugins: [
+  //   new webpack.ProvidePlugin({
+  //     THREE: "three"
+  //   })
+  // ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["env"] }
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["env"] }
+          },
+          {
+            loader: "ts-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
