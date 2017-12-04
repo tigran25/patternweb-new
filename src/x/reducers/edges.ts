699d5dc8e241bbcdf4241ee0155c17af055765eb
John Rees <johnrees@users.noreply.github.com>
2017-12-04 17:33:57 +0000

    edges

--- src/x/reducers/edges.ts
+++ src/x/reducers/edges.ts
@@ -1,9 +1,18 @@
 import { ADD_EDGE } from "../actions";
 
+const edge = (state, action) => {
+  switch(action.type) {
+    case ADD_EDGE:
+      return [action.source, action.target];
+    default:
+      return state;
+  }
+}
+
 const edges = (state = [], action) => {
   switch (action.type) {
     case ADD_EDGE:
-      return [...state, [action.source, action.target]];
+      return [...state, edge(undefined, action)];
     default:
       return state;
   }
